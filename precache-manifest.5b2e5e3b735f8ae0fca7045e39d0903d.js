d2e4344c4390ff7ce0c5de4585beb2b4e841da1f
junlico <junl14@uci.edu>
2018-12-09 00:07:04 -0800

    Updates

--- precache-manifest.5b2e5e3b735f8ae0fca7045e39d0903d.js
+++ precache-manifest.5b2e5e3b735f8ae0fca7045e39d0903d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/dragalia-lost/static/js/runtime~main.79c16a3f.js"
   },
   {
-    "revision": "9d9fb81b7edebe3943ed",
-    "url": "/dragalia-lost/static/js/main.9d9fb81b.chunk.js"
+    "revision": "1504169fcf7e848e1c9c",
+    "url": "/dragalia-lost/static/js/main.1504169f.chunk.js"
   },
   {
     "revision": "1d0d82cd00464520e4a5",
     "url": "/dragalia-lost/static/js/1.1d0d82cd.chunk.js"
   },
   {
-    "revision": "9d9fb81b7edebe3943ed",
+    "revision": "1504169fcf7e848e1c9c",
     "url": "/dragalia-lost/static/css/main.1e9f3adf.chunk.css"
   },
   {
-    "revision": "31842675e61f2ac538e43848e4ad9074",
+    "revision": "0f128602e5d4834c80262173d706c6ed",
     "url": "/dragalia-lost/index.html"
   }
 ];
