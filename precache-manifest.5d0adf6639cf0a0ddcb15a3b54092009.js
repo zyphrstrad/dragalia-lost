ea591e6778f31672a3aa76707f62a858fe491fdd
junlico <junl14@uci.edu>
2019-01-17 03:57:30 -0800

    Updates

---
+++ precache-manifest.5d0adf6639cf0a0ddcb15a3b54092009.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79c16a3f347e165d130d",
+    "url": "/dragalia-lost/static/js/runtime~main.79c16a3f.js"
+  },
+  {
+    "revision": "8b2d64aa69a1e2e85b10",
+    "url": "/dragalia-lost/static/js/main.8b2d64aa.chunk.js"
+  },
+  {
+    "revision": "df0e7d2484a1fd9795da",
+    "url": "/dragalia-lost/static/js/1.df0e7d24.chunk.js"
+  },
+  {
+    "revision": "8b2d64aa69a1e2e85b10",
+    "url": "/dragalia-lost/static/css/main.6f0ff3c0.chunk.css"
+  },
+  {
+    "revision": "465d1205fe06332105e2dda883a18d6a",
+    "url": "/dragalia-lost/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dragalia-lost/precache-manifest.cbbeab05a1add10764289004294f37cc.js"
+  "/dragalia-lost/precache-manifest.5d0adf6639cf0a0ddcb15a3b54092009.js"
 );
 
 workbox.clientsClaim();
