7d25a804ec6b55c9359211b049b723427e583796
junlico <junl14@uci.edu>
2019-02-19 10:27:49 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dragalia-lost/precache-manifest.ef256990a8ceb7e6ea4a8c6a41d0858f.js"
+  "/dragalia-lost/precache-manifest.4a70e75c1d95220bb9b26587f6ca4ffd.js"
 );
 
 workbox.clientsClaim();
