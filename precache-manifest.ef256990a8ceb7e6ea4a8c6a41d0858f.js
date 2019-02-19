63df19659b9202370fe4389d1f7c4967e2a74e16
junlico <junl14@uci.edu>
2019-02-19 10:20:39 -0800

    Updates

--- precache-manifest.ef256990a8ceb7e6ea4a8c6a41d0858f.js
+++ precache-manifest.ef256990a8ceb7e6ea4a8c6a41d0858f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/dragalia-lost/static/js/runtime~main.79c16a3f.js"
   },
   {
-    "revision": "7cbec3d90125c74e2d87",
-    "url": "/dragalia-lost/static/js/main.7cbec3d9.chunk.js"
+    "revision": "058087f0812f75841f2b",
+    "url": "/dragalia-lost/static/js/main.058087f0.chunk.js"
   },
   {
     "revision": "22c5aec636e213cfaafe",
     "url": "/dragalia-lost/static/js/1.22c5aec6.chunk.js"
   },
   {
-    "revision": "7cbec3d90125c74e2d87",
+    "revision": "058087f0812f75841f2b",
     "url": "/dragalia-lost/static/css/main.3e0aec57.chunk.css"
   },
   {
-    "revision": "e896a3bf90ec017f912fabe138553453",
+    "revision": "6ca4dfe7eadd938dcf0aebedd2fcdde2",
     "url": "/dragalia-lost/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dragalia-lost/precache-manifest.cf78be1a828804575e5bee4814ec42ac.js"
+  "/dragalia-lost/precache-manifest.ef256990a8ceb7e6ea4a8c6a41d0858f.js"
 );
 
 workbox.clientsClaim();
