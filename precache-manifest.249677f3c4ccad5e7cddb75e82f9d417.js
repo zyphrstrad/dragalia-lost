5437304de4f368a7c5215bfddb0d3ef96d45187b
junlico <sean.junlico@gmail.com>
2019-04-18 18:39:37 -0700

    Updates

--- precache-manifest.249677f3c4ccad5e7cddb75e82f9d417.js
+++ precache-manifest.249677f3c4ccad5e7cddb75e82f9d417.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/dragalia-lost/static/js/runtime~main.2eb4cee8.js"
   },
   {
-    "revision": "1b05e139e6e67f25671c",
-    "url": "/dragalia-lost/static/js/main.0fa60c96.chunk.js"
+    "revision": "2f6cba1e317e727a7e65",
+    "url": "/dragalia-lost/static/js/main.8d3ee2d0.chunk.js"
   },
   {
     "revision": "0409eea8bbe1bf250aa7",
     "url": "/dragalia-lost/static/js/2.4de5b2f8.chunk.js"
   },
   {
-    "revision": "1b05e139e6e67f25671c",
+    "revision": "2f6cba1e317e727a7e65",
     "url": "/dragalia-lost/static/css/main.a9b9ec23.chunk.css"
   },
   {
-    "revision": "d25b133f43e60e201e716cdb2a3cad2a",
+    "revision": "6c1a5efecf1de101744c6c94022fdd44",
     "url": "/dragalia-lost/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dragalia-lost/precache-manifest.d522b54a7e57eda76318213b55ef20e2.js"
+  "/dragalia-lost/precache-manifest.249677f3c4ccad5e7cddb75e82f9d417.js"
 );
 
 workbox.clientsClaim();
