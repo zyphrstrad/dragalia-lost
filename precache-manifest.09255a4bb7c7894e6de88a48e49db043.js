89225ebc28538df68fa62f91a866cb56404abec6
junlico <sean.junlico@gmail.com>
2019-04-23 14:47:57 -0700

    Updates

---
+++ precache-manifest.09255a4bb7c7894e6de88a48e49db043.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095d5d5235ccb04cef5151115ce23903",
+    "url": "/dragalia-lost/index.html"
+  },
+  {
+    "revision": "bcc08c3d1a9538948cd3",
+    "url": "/dragalia-lost/static/css/main.3f315366.chunk.css"
+  },
+  {
+    "revision": "54831e5eb2dd237fcd10",
+    "url": "/dragalia-lost/static/js/2.5d239645.chunk.js"
+  },
+  {
+    "revision": "bcc08c3d1a9538948cd3",
+    "url": "/dragalia-lost/static/js/main.f415cc48.chunk.js"
+  },
+  {
+    "revision": "3a9e9a4a212d25debc68",
+    "url": "/dragalia-lost/static/js/runtime~main.c60f60f1.js"
+  }
+]);
